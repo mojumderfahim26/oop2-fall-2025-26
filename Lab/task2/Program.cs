f0a46baaa791e708dc099711c3b989965512a761
mojumderfahim26 <mojumderfahim26@gmail.com>
2025-12-02 10:51:54 -0800

    23-50691-1

---
+++ Lab/task2/Program.cs
@@ -0,0 +1,7 @@
+﻿int price = 250;
+Console.WriteLine($"The price is {price} $.");
+Console.WriteLine($"Discount is 15%.");
+
+float final_price = price * 0.15f;
+
+Console.WriteLine($"The final price after discount is ${final_price} .");
