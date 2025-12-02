f50398da88b9241a857250c7c459095345216139
mojumderfahim26 <mojumderfahim26@gmail.com>
2025-12-02 11:02:46 -0800

    23-50691-1

---
+++ Lab/task 3/Program.cs
@@ -0,0 +1,5 @@
+﻿float temperatureC = 40.5f;
+float temperatureF = (temperatureC * 9 / 5) + 32;
+
+Console.WriteLine($"Temperature in Celsius: {temperatureC}°C");
+Console.WriteLine($"Temperature in Fahrenheit: {temperatureF}°F");
