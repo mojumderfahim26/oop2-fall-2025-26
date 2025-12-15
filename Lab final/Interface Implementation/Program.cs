a0e90c281188b4774c6b0a73a0301d1562faec02
mojumderfahim26 <mojumderfahim26@gmail.com>
2025-12-15 12:55:27 -0800

    23-50691-1

---
+++ Lab final/Interface Implementation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IPrintable
+{
+    void Print();
+}
+
+interface ISerializable
+{
+    void Serialize();
+}
+
+class Report : IPrintable, ISerializable
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing data...");
+    }
+
+    public void Serialize()
+    {
+        Console.WriteLine("Serializing data...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Report r1 = new Report();
+        r1.Print();
+        r1.Serialize();
+    }
+}
